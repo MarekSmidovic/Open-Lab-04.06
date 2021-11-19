e7362f6e1415411a7ecb3e5ed47e89bd20c43e31
smidovic <msmidovic@gmail.com>
2021-11-19 11:14:09 +0100

    idstranovanie nerparnych cisel

--- Open-Lab-04.06/Numbers.cs
+++ Open-Lab-04.06/Numbers.cs
@@ -1,12 +1,25 @@
 ﻿using System;
-
+using System.Linq;
 namespace Open_Lab_04._06
 {
+    
     public class Numbers
     {
         public int[] NoOdds(int[] numbers)
-        {
-            throw new NotImplementedException();
+        {           
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int a = numbers[i] % 2;
+                int b = numbers[i];
+                if (a == 1)
+                {
+                    int indexToRemove = i;
+                    numbers = numbers.Where((source, index) => index != indexToRemove).ToArray();
+                 }                                                            
+            }
+
+            return numbers;
+
         }
     }
 }
